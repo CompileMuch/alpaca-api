0f86b636c57e93c8475e7855565856ee211f5972
jms2 <joe@seven2.com>
2023-03-27 09:49:00 -0700

    add chat example

---
+++ index.js
@@ -0,0 +1,33 @@
+import { LLamaClient } from "llama-node";
+import path from "path";
+require('dotenv').config()
+
+const model = path.resolve(process.cwd(), process.env.ALPACA_PATH);
+
+const llama = new LLamaClient(
+    {
+        path: model,
+        numCtxTokens: 128,
+    },
+    true
+);
+
+const content = "how are you?";
+
+llama.createChatCompletion(
+    {
+        messages: [{ role: "user", content }],
+        numPredict: BigInt(128),
+        temp: 0.2,
+        topP: 1,
+        topK: BigInt(40),
+        repeatPenalty: 1,
+        repeatLastN: BigInt(64),
+        seed: BigInt(0),
+    },
+    (response) => {
+        if (!response.completed) {
+            process.stdout.write(response.token);
+        }
+    }
+);
