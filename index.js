5f14f5ab6f1a5e62f5b4dea17fbefa66af28e17e
jms2 <joe@seven2.com>
2023-03-27 13:49:11 -0700

    add

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
+const Dalai = require('dalai')
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from the API!' });
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+async function requestWithAwait() {
+  return new Promise((resolve, reject) => {
+    new Dalai().request({
+      model: "7B",
+      prompt: "The following is a conversation between a boy and a girl:",
+    }, (token) => {
+      if (token) {
+        resolve(token);
+      } else {
+        reject(new Error('Request failed'));
+      }
+    });
+  });
+}
+
+app.get('/', async (req, res) => {
+  try {
+    const token = await requestWithAwait();
+    process.stdout.write(token);
+    res.json({ message: 'Hello from the API!', token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error occurred while processing the request' });
+  }
 });
 
 app.listen(PORT, () => {
